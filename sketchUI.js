7337a7bc6275ee354524e43ebb903dfff7195834
osseko <253503304+osseko@users.noreply.github.com>
2026-03-02 21:50:39 +0800

    box now had color randomizer

--- sketchUI.js
+++ sketchUI.js
@@ -1,22 +1,32 @@
 //box with button or div padding
+
 const headTitle = document.createElement("h1");
         headTitle.textContent = "Etch-a-Sketch"
         headTitle.style.margin = "30px"
+        headTitle.style.padding = "20px"
 
 
 
 const gridCont = document.querySelector("#gridmaster");
-       
+
+
 
 const boxOver = document.createElement("box");
     
             boxOver.innerText = ""
                   
-            boxOver.style.padding = "50px 50px 50px 50px"
-        
-            boxOver.addEventListener("mousemove", () => {
-                boxOver.style.backgroundColor = "blue"
+            boxOver.style.padding = "20px 30px 20px 30px"
+            boxOver.style.border = "solid black"        
+            boxOver.addEventListener("mouseenter", () => {
                 
+                function getRandomColor(){
+                     const col =  "#"+Math.random().toString(16).slice(-6)
+                    return col;
+                }
+                const randomColor = getRandomColor();
+
+                boxOver.style.backgroundColor = randomColor;
+                console.log("color", randomColor)
             })
 
             boxOver.addEventListener("mouseleave", () => {
